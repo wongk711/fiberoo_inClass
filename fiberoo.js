102c7bd4c10e9ff15bd4889831e049984615b435
Kameron Wong <wongk711@hawaii.edu>
2020-02-11 11:53:08 -1000

    Initial commit

---
+++ fiberoo.js
@@ -0,0 +1,19 @@
+function fib(size) {
+  const ret = [0, 1];
+  for (let i = 1; i < size - 1; i++) {
+    ret.push(ret[i] + ret[i - 1]);
+  }
+  return ret;
+}
+
+function numToStrings(numList){
+  return _.map(numList, (num) => `${num}`);
+}
+
+function numEvenNumbers(list){
+  return (_.filter(list, (num) => num % 2 === 0)).length;
+}
+
+console.log(fib(50));
+console.log(numToStrings(fib(10)));
+console.log(numEvenNumbers(fib(50)));
